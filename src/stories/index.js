432518aaf4fb6ee89918bada1dc5577b337c6692
ian <ichsaras@gmail.com>
2018-02-19 17:24:52 +0100

    - created routing for page transitions - created Fade page transition option - still need to find out how I can set context to the story using .addDecorator

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,9 +4,12 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, number } from '@storybook/addon-knobs/react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import ImageLoad from '../components/ImageLoad';
 import Button from '../components/Button';
+import { RouteSwitch, DemoLayout, RouterLayout } from '../components/router.demo';
+import PageTransition from '../components/PageTransition';
 
 storiesOf('Images', module)
     .addDecorator(withKnobs)
@@ -17,10 +20,39 @@ storiesOf('Images', module)
             alt={text('alt', 'Image alt text')}
             ratio={number('ratio', 16 / 9)}
             color={text('color', '#222')}
-            onLoad={() => { console.log('onload'); }}
-            onError={() => { console.log('onError'); }}
+            onLoad={() => {
+                console.log('onload');
+            }}
+            onError={() => {
+                console.log('onError');
+            }}
         />
     ));
 
 storiesOf('Buttons', module)
+    .addDecorator(withKnobs)
     .addWithJSX('Button', () => <Button>{text('label', 'Click me good!')}</Button>);
+
+storiesOf('PageTransitions', module)
+    .addDecorator(withKnobs)
+    .addDecorator(RouterLayout)
+    .addWithJSX('fade', (props) => (
+        <PageTransition animation={text('animation', 'fade')} backgroundColor={text('background-color', '#ddd')}>
+            <RouteSwitch test={props}/>
+        </PageTransition>
+    ));
+
+storiesOf('full transition test', module)
+    .addDecorator(withKnobs)
+    .addWithJSX('fade test', () => (
+        <BrowserRouter>
+            <Route render={({ location }) => (
+                <DemoLayout>
+                    <PageTransition>
+                        <RouteSwitch location={location}/>
+                    </PageTransition>
+                </DemoLayout>
+            )
+            }/>
+        </BrowserRouter>
+    ));
